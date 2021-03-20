8adcd0087102ee8b8763ded977acc76307b282bf
Thiago de Castro <thiago.castro@talkdesk.com>
2021-03-20 17:25:10 +0000

    added waits

--- src/tests/example.js
+++ src/tests/example.js
@@ -1,25 +1,25 @@
 describe('First Steps with WebdriverIO', () => {
 	it('Load Example Website', () => {
 		browser.url('http://www.example.com')
-
-		browser.pause(1000)
-
-		expect(browser).toHaveUrl('http://www.example.com/')
+		expect(browser).toHaveUrl('http://www.example.com/') // There is an implicit wait here.
 		expect(browser).toHaveTitle('Example Domain')
 	})
 
 	it('H1 should be visible', () => {
 		const h1 = $('h1')
+		h1.waitForExist() // Always wait for the element before using it.
 		expect(h1).toBeVisible()
 	})
 
 	it('Description should be visible', () => {
 		const p = $('p')
+		p.waitForExist() // Always wait for the element before using it.
 		expect(p).toBeVisible()
 	})
 
 	it('Check link value', () => {
 		const link = $('a')
-		expect(link).toHaveLink("https://www.iana.org/domains/example")
+		link.waitForExist() // Always wait for the element before using it.
+		expect(link).toHaveLink('https://www.iana.org/domains/example')
 	})
 })
