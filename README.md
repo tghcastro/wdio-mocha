0891f03241012514af473b9ea194b110189c5cc1
Thiago de Castro <thiago.castro@talkdesk.com>
2021-03-20 16:32:43 +0000

    fixing code style before commits with husky, prettier and pretty-quick

--- README.md
+++ README.md
@@ -12,10 +12,16 @@ npm install --save-dev @wdio/cli
 npx wdio config
 
 # Prettier
-npm install --save-dev prettier 
+npm install --save-dev prettier
+npm install husky --save-dev && npx husky init
+npm install --save-dev pretty-quick
 
 # Babel
 npm install --save-dev @babel/core @babel/cli @babel/preset-env @babel/register
+```
 
+## "Cleaning code"
 
+```shell_script
+npm run pretty-quick
 ```
--- src/tests/example.js
+++ src/tests/example.js
@@ -1,12 +1,10 @@
 describe('First Steps with WebdriverIO', () => {
-    
-    it('Load Example Website', () => {
-        browser.url('http://www.example.com')
+	it('Load Example Website', () => {
+		browser.url('http://www.example.com')
 
-        browser.pause(2000);
+		browser.pause(2000)
 
-        expect(browser).toHaveUrl('http://www.example.com/')
-        expect(browser).toHaveTitle('Example Domain')
-
-    })
-});
+		expect(browser).toHaveUrl('http://www.example.com/')
+		expect(browser).toHaveTitle('Example Domain')
+	})
+})
