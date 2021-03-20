817e074ee7c94d6451e80e5480032ebe33bbcb9d
Thiago de Castro <thiago.castro@talkdesk.com>
2021-03-20 16:24:05 +0000

    added first expectation

--- src/tests/example.js
+++ src/tests/example.js
@@ -1,13 +1,12 @@
 describe('First Steps with WebdriverIO', () => {
+    
     it('Load Example Website', () => {
         browser.url('http://www.example.com')
 
         browser.pause(2000);
-    })
 
-    it('Load WebdriverIO website', () => {
-        browser.url('https://webdriver.io')
-        browser.pause(2000)
-    })
+        expect(browser).toHaveUrl('http://www.example.com/')
+        expect(browser).toHaveTitle('Example Domain')
 
+    })
 });
