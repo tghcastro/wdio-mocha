45214a260e3677f3b294cc6794398d113477a220
Thiago de Castro <thiago.castro@talkdesk.com>
2021-03-20 16:21:18 +0000

    pausing execution

--- src/tests/example.js
+++ src/tests/example.js
@@ -1,5 +1,13 @@
 describe('First Steps with WebdriverIO', () => {
     it('Load Example Website', () => {
         browser.url('http://www.example.com')
+
+        browser.pause(2000);
     })
+
+    it('Load WebdriverIO website', () => {
+        browser.url('https://webdriver.io')
+        browser.pause(2000)
+    })
+
 });
